151266312e8bf77d88db0e849f787dad22010691
Vijay Singh <vijaysingh3225@gmail.com>
2024-07-30 17:09:07 -0400

    Initial Commit

---
+++ src/test/kotlin/quest/marketstack/TradingApp/TradingAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package quest.marketstack.TradingApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradingAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
